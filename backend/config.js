671bd85df23e41f192c42a3e4804058a6f09b2ca
Nisal Fonseka <nisalfonseka@gmail.com>
2024-08-21 15:07:08 +0530

    bakcned

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+//export const mongoDBURL = "mongodb+srv://nisal:nisal123@cluster0.p860x.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
+export const mongoDBURL = "mongodb+srv://pererabinuri29:20020529@cluster0.vnoir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
---
+++ backend/models/bookModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    vehicleType: {
+      type: String,
+      required: true,
+    },
+  
+    vehicleNumber: {
+      type: String,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: false,
+      
+    },
+
+    selectedServices: {
+      type: [String],
+      required: false,
+      
+    },
+    
+
+    totalCost: {
+      type: String,
+      required: false,
+      
+    },
+
+    totalTime: {
+      type: String,
+      required: false,
+      
+    },
+
+    selectedDate: {
+      type: String,
+      required: false,
+      
+    },
+
+    selectedTimeSlot: {
+      type: String,
+      required: false,
+      
+    },
+    timeSlotsAvailability: {
+      type: String,
+      required: false,
+      
+    },
+   
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model("cat", bookSchema);
